470337eb2cc37d23d0512e16380b24cc50e2aba5
kevvnbk <kevinbaek01@gmail.com>
2025-03-11 07:25:40 +0000

    feat: add faiss index retrieval

---
+++ src/retriever/remove_duplicates.py
@@ -0,0 +1,12 @@
+import json
+from collections import Counter
+
+with open("faiss_data/chunked_dataset.json", "r") as f:
+    data = json.load(f)
+
+unique_entries = list(dict.fromkeys(data))
+
+with open("chunked_dataset_no_duplicates.json", "w") as f:
+    json.dump(unique_entries, f, indent=4)
+
+print(f"Removed duplicates. Original count: {len(data)}, Unique count: {len(unique_entries)}")
